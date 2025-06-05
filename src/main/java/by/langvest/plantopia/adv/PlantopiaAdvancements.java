b57fec9bffcc1402d567d25f656e49991077ad53
LanGvest <mister.langvest@gmail.com>
2025-06-05 14:15:11 +0300

    ✨ Add cobblestone shard pet advancement

--- src/main/java/by/langvest/plantopia/adv/PlantopiaAdvancements.java
+++ src/main/java/by/langvest/plantopia/adv/PlantopiaAdvancements.java
@@ -17,6 +17,7 @@ public class PlantopiaAdvancements {
 	public static final PlantopiaAdvancement ROOT = registerAdvancement("root", PlantopiaAdvancement::new, MetaProperties.copy(MetaType.ROOT).group(Plantopia.MOD_ID).background("dirt").icon(PlantopiaBlocks.FIREWEED));
 	public static final PlantopiaAdvancement COLLECT_ALL_FLOWERS = registerAdvancement("collect_all_flowers", PlantopiaAdvancement::new, MetaProperties.copy(MetaType.CHALLENGE).parent(ROOT).hidden().icon(PlantopiaItems.FLOWERS_ICON));
 	public static final PlantopiaAdvancement PLACE_HOGWEED = registerAdvancement("place_hogweed", PlantopiaAdvancement::new, MetaProperties.copy(MetaType.CHILD).parent(ROOT).hidden().icon(PlantopiaBlocks.HOGWEED));
+	public static final PlantopiaAdvancement PLACE_COBBLESTONE_SHARD_PET = registerAdvancement("place_cobblestone_shard_pet", PlantopiaAdvancement::new, MetaProperties.copy(MetaType.CHILD).parent(ROOT).hidden().icon(PlantopiaBlocks.COBBLESTONE_SHARD));
 
 	public static @NotNull PlantopiaAdvancement registerAdvancement(String name, @NotNull Supplier<PlantopiaAdvancement> supplier, @NotNull MetaProperties metaProperties) {
 		var id = plantopiaLocationFrom(name);
