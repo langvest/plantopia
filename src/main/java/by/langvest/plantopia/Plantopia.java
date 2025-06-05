7008cd6eeeff64c0ea029dd3199c8ee928494604
LanGvest <mister.langvest@gmail.com>
2025-06-05 19:43:39 +0300

    ✨ Add the ability to throw cobblestone shards

--- src/main/java/by/langvest/plantopia/Plantopia.java
+++ src/main/java/by/langvest/plantopia/Plantopia.java
@@ -5,6 +5,7 @@ import by.langvest.plantopia.block.PlantopiaCompats;
 import by.langvest.plantopia.block.entity.PlantopiaBlockEntities;
 import by.langvest.plantopia.client.color.PlantopiaColors;
 import by.langvest.plantopia.client.render.PlantopiaRenderTypes;
+import by.langvest.plantopia.entity.PlantopiaEntities;
 import by.langvest.plantopia.item.PlantopiaItems;
 import by.langvest.plantopia.particle.PlantopiaParticleTypes;
 import by.langvest.plantopia.tab.PlantopiaCreativeModeTabs;
@@ -40,6 +41,7 @@ public final class Plantopia {
 		PlantopiaTreeDecoratorTypes.setup(bus);
 		PlantopiaItems.setup(bus);
 		PlantopiaBlockEntities.setup(bus);
+		PlantopiaEntities.setup(bus);
 	}
 
 	private void commonSetup(final @NotNull FMLCommonSetupEvent event) {
--- src/main/java/by/langvest/plantopia/item/special/PlantopiaCobblestoneShardBlockItem.java
+++ src/main/java/by/langvest/plantopia/item/special/PlantopiaCobblestoneShardBlockItem.java
@@ -1,7 +1,17 @@
 package by.langvest.plantopia.item.special;
 
+import by.langvest.plantopia.entity.special.PlantopiaCobblestoneShardProjectileEntity;
+import by.langvest.plantopia.item.PlantopiaItems;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
@@ -30,5 +40,29 @@ public class PlantopiaCobblestoneShardBlockItem extends BlockItem {
 		}
 
 		return super.getPlacementState(context);
+	}
+
+	public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
+		var itemStack = player.getItemInHand(hand);
+
+		level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+
+		player.getCooldowns().addCooldown(PlantopiaItems.COBBLESTONE_SHARD.get(), 20);
+		player.getCooldowns().addCooldown(PlantopiaItems.MOSSY_COBBLESTONE_SHARD.get(), 20);
+
+		if(!level.isClientSide()) {
+			var projectileEntity = new PlantopiaCobblestoneShardProjectileEntity(level, player);
+			projectileEntity.setItem(itemStack);
+			projectileEntity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+			level.addFreshEntity(projectileEntity);
+		}
+
+		player.awardStat(Stats.ITEM_USED.get(this));
+
+		if(!player.getAbilities().instabuild) {
+			itemStack.shrink(1);
+		}
+
+		return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
 	}
 }
