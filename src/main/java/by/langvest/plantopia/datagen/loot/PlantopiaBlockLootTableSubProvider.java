c757cfb9e8a8506e7a276be9e2ecd3bc5673aed8
LanGvest <mister.langvest@gmail.com>
2025-07-17 23:04:10 +0300

    🗑️ Remove seeds drop from the watergrass plant

--- src/main/java/by/langvest/plantopia/datagen/loot/PlantopiaBlockLootTableSubProvider.java
+++ src/main/java/by/langvest/plantopia/datagen/loot/PlantopiaBlockLootTableSubProvider.java
@@ -71,7 +71,6 @@ public class PlantopiaBlockLootTableSubProvider extends BlockLootSubProvider {
 
 		add(PlantopiaBlocks.GIANT_GRASS.get(), block -> createTriplePlantWithSeedDrops(block, Blocks.GRASS, Items.WHEAT_SEEDS));
 		add(PlantopiaBlocks.GIANT_FERN.get(), block -> createTriplePlantWithSeedDrops(block, Blocks.FERN, Items.WHEAT_SEEDS));
-		add(PlantopiaBlocks.WATERGRASS.get(), this::createWatergrassDrops);
 		add(PlantopiaBlocks.CLOVER.get(), this::createCloverDrops);
 		add(PlantopiaBlocks.AZOLLA.get(), this::createAzollaDrops);
 		add(PlantopiaBlocks.COBBLESTONE_SHARD.get(), this::createCobblestoneShardDrops);
@@ -261,17 +260,6 @@ public class PlantopiaBlockLootTableSubProvider extends BlockLootSubProvider {
 			.when(HAS_SHEARS);
 
 		return createBlockTable(block, lootEntry);
-	}
-
-	private LootTable.@NotNull Builder createWatergrassDrops(Block block) {
-		LootPoolEntryContainer.Builder<?> lootEntry = item(block)
-			.when(HAS_SHEARS)
-			.otherwise(
-				withSurvivesExplosionCondition(block, item(Items.WHEAT_SEEDS))
-					.when(randomChance(SEEDS_CHANCE))
-			);
-
-		return createDoubleHighPlantTable(block, lootEntry);
 	}
 
 	private LootTable.@NotNull Builder createCobblestoneShardDrops(Block block) {
