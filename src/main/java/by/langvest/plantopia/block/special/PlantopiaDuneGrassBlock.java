78554e93148e6e74e8323d146165cf10af312379
LanGvest <mister.langvest@gmail.com>
2025-07-18 13:34:03 +0300

    🩹 Fix dune grass shape

--- src/main/java/by/langvest/plantopia/block/special/PlantopiaDuneGrassBlock.java
+++ src/main/java/by/langvest/plantopia/block/special/PlantopiaDuneGrassBlock.java
@@ -8,15 +8,26 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class PlantopiaDuneGrassBlock extends BushBlock implements BonemealableBlock {
+	protected static final VoxelShape SHAPE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 13.0D, 14.0D);
+
 	public PlantopiaDuneGrassBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+		return SHAPE;
 	}
 
 	@Override
