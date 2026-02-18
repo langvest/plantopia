d57842826353500cede5ca16086fc5d9dcc5e5b6
LanGvest <mister.langvest@gmail.com>
2026-02-18 10:01:48 +0300

    ✏️ Update advancement descriptions

--- src/main/java/by/langvest/plantopia/datagen/lang/PlantopiaLanguageProvider.java
+++ src/main/java/by/langvest/plantopia/datagen/lang/PlantopiaLanguageProvider.java
@@ -34,14 +34,14 @@ public class PlantopiaLanguageProvider extends LanguageProvider {
 
 		tab(PlantopiaCreativeModeTabs.MAIN, "Plantopia");
 
-		advancement(PlantopiaAdvancements.ROOT, "Plantopia", "What a wonderful world!");
+		advancement(PlantopiaAdvancements.ROOT, "Plantopia", "A plant lover's utopia!");
 		advancement(PlantopiaAdvancements.COLLECT_ALL_FLOWERS, "A Million-Dollar Bouquet", "Complete your flower collection");
 		advancement(PlantopiaAdvancements.COLLECT_ALL_HERBS, "Local Herbalist", "Gather every herb under the sun... almost");
 		advancement(PlantopiaAdvancements.COLLECT_ALL_MUSHROOMS, "Certified 'Shroom Enjoyer", "Become a true connoisseur of fungi");
 		advancement(PlantopiaAdvancements.PLACE_HOGWEED, "Secret Agent of Hogweed", "Do your humble part to help Hogweed take over the world!");
-		advancement(PlantopiaAdvancements.PLACE_COBBLESTONE_SHARD_PET, "Rock Solid Friendship", "Craft a friend who will never run away... because it can't move");
-		advancement(PlantopiaAdvancements.WALK_ON_QUICKSAND_WITH_LEATHER_BOOTS, "Antigravity Tricks", "Walk on Quicksand... like a great illusionist");
-		advancement(PlantopiaAdvancements.PLUCK_LUCKY_DAISY_PETAL, "Loves me, loves me not", "Ask the Lucky Daisy and pluck your fate!");
+		advancement(PlantopiaAdvancements.PLACE_COBBLESTONE_SHARD_PET, "Rock Solid Friendship", "Get yourself a friend who will never run away... because it can't move!");
+		advancement(PlantopiaAdvancements.WALK_ON_QUICKSAND_WITH_LEATHER_BOOTS, "Antigravity Tricks", "Walk on Quicksand... like a greatest illusionist!");
+		advancement(PlantopiaAdvancements.PLUCK_LUCKY_DAISY_PETAL, "Loves me, loves me not", "Ask the Lucky Daisy and pluck your fate");
 		advancement(PlantopiaAdvancements.OBTAIN_TANSY, "Tansy? You mean fancy?!", "No time to explain... just keep going");
 
 		damageType(PlantopiaDamageTypes.THORNY_SHRUB, "%1$s was poked to death by a thorny shrub");
