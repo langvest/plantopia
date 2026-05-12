33276ab03de995c8e6754212bc32c8cf46206c59
LanGvest <mister.langvest@gmail.com>
2026-05-12 21:21:09 +0300

    ✏️ Update PlantopiaEntityLayerDefinitions

--- src/main/java/by/langvest/plantopia/client/render/PlantopiaEntityLayerDefinitions.java
+++ src/main/java/by/langvest/plantopia/client/render/PlantopiaEntityLayerDefinitions.java
@@ -1,17 +1,17 @@
 package by.langvest.plantopia.client.render;
 
 import by.langvest.toolkit.event.client.RegisterLayerDefinitionsEvent;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Lists;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class PlantopiaEntityLayerDefinitions {
-    private static final Set<Pair<ModelLayerLocation, Supplier<LayerDefinition>>> DEFINITIONS = Sets.newHashSet();
+    private static final List<Pair<ModelLayerLocation, Supplier<LayerDefinition>>> DEFINITIONS = Lists.newArrayList();
 
     public static void registerLayerDefinition(ModelLayerLocation layerLocation, Supplier<LayerDefinition> supplier) {
         DEFINITIONS.add(Pair.of(layerLocation, supplier));
--- src/main/java/by/langvest/toolkit/event/client/RegisterLayerDefinitionsEvent.java
+++ src/main/java/by/langvest/toolkit/event/client/RegisterLayerDefinitionsEvent.java
@@ -5,14 +5,14 @@ import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
+import java.util.Collection;
 import java.util.function.Supplier;
 
 public abstract class RegisterLayerDefinitionsEvent extends ClientEvent {
 	public static abstract class EntityEvent extends RegisterLayerDefinitionsEvent {
 		public abstract void register(ModelLayerLocation layerLocation, Supplier<LayerDefinition> supplier);
 
-		public void registerAll(@NotNull Set<Pair<ModelLayerLocation, Supplier<LayerDefinition>>> definitions) {
+		public void registerAll(@NotNull Collection<Pair<ModelLayerLocation, Supplier<LayerDefinition>>> definitions) {
 			for(var definition : definitions) {
 				register(definition.getFirst(), definition.getSecond());
 			}
