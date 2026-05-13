7d8d6096339883197ff8aff18fc82ea658546747
LanGvest <mister.langvest@gmail.com>
2026-05-13 14:18:54 +0300

    🧪 Inject custom strip logit to special types of blocks

--- src/main/java/by/langvest/plantopia/block/PlantopiaBlocks.java
+++ src/main/java/by/langvest/plantopia/block/PlantopiaBlocks.java
@@ -17,6 +17,7 @@ import by.langvest.plantopia.meta.property.PlantopiaOrderType;
 import by.langvest.plantopia.meta.property.PlantopiaTintType;
 import by.langvest.toolkit.registry.SupposedRegistryObject;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffects;
@@ -25,6 +26,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.MapColor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -40,8 +42,8 @@ import static by.langvest.plantopia.util.helper.PlantopiaResourceHelper.plantopi
  * @see net.minecraft.world.level.block.Blocks
  */
 public class PlantopiaBlocks {
-	public static final RegistryObject<Block> BIRCH_BASE_LOG = registerBlock("birch_base_log", properties -> new PlantopiaBirchBaseBlock(() -> Blocks.STRIPPED_BIRCH_LOG, properties), MetaProperties.of(MetaType.LOG).order(PlantopiaOrderType.BIRCH).mapColor(PlantopiaBirchBaseBlock::logMapColor).customModel().customDrop());
-	public static final RegistryObject<Block> BIRCH_BASE_WOOD = registerBlock("birch_base_wood", properties -> new PlantopiaBirchBaseBlock(() -> Blocks.STRIPPED_BIRCH_WOOD, properties), MetaProperties.of(MetaType.WOOD).order(PlantopiaOrderType.BIRCH).mapColor(PlantopiaBirchBaseBlock::woodMapColor).customModel().customDrop());
+	public static final RegistryObject<Block> BIRCH_BASE_LOG = registerBlock("birch_base_log", PlantopiaDirectionalPillarBlock::new, MetaProperties.of(MetaType.LOG).order(PlantopiaOrderType.BIRCH).mapColor(birchBaseLogMapColor()).customModel().customDrop().strippable(() -> Blocks.STRIPPED_BIRCH_LOG));
+	public static final RegistryObject<Block> BIRCH_BASE_WOOD = registerBlock("birch_base_wood", PlantopiaDirectionalPillarBlock::new, MetaProperties.of(MetaType.WOOD).order(PlantopiaOrderType.BIRCH).mapColor(birchBaseWoodMapColor()).customModel().customDrop().strippable(() -> Blocks.STRIPPED_BIRCH_WOOD));
 
 	public static final RegistryObject<Block> INFESTED_GRASS_BLOCK = registerBlock("infested_grass_block", PlantopiaInfestedGrassBlock::new, MetaProperties.of(MetaType.GRASS_BLOCK).customModel().customDrop());
 	public static final RegistryObject<Block> INFESTED_DIRT = registerBlock("infested_dirt", PlantopiaInfestedDirtBlock::new, MetaProperties.of(MetaType.DIRT).randomlyTicking().customModel().customDrop());
---
+++ src/main/java/by/langvest/plantopia/mixin/item/special/PlantopiaAxeItemMixin.java
@@ -0,0 +1,71 @@
+package by.langvest.plantopia.mixin.item.special;
+
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Mixin(AxeItem.class)
+public abstract class PlantopiaAxeItemMixin {
+    @Shadow
+    public static Map<Block, Block> STRIPPABLES;
+
+    @Inject(
+        method = "getAxeStrippingState(Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/level/block/state/BlockState;",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false,
+        require = 1
+    )
+    private static void getAxeStrippingState(@NotNull BlockState unstrippedState, CallbackInfoReturnable<BlockState> cir) {
+        Block strippedBlock = STRIPPABLES.get(unstrippedState.getBlock());
+
+        if (strippedBlock != null && unstrippedState.hasProperty(BlockStateProperties.FACING)) {
+            var newState = plantopia$getSpecialStrippedState(unstrippedState, strippedBlock);
+
+            if (newState != null) {
+                cir.setReturnValue(newState);
+            }
+        }
+    }
+
+    @Inject(
+        method = "getStripped(Lnet/minecraft/world/level/block/state/BlockState;)Ljava/util/Optional;",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void getStripped(@NotNull BlockState unstrippedState, CallbackInfoReturnable<Optional<BlockState>> cir) {
+        Block strippedBlock = STRIPPABLES.get(unstrippedState.getBlock());
+
+        if (strippedBlock != null) {
+            var newState = plantopia$getSpecialStrippedState(unstrippedState, strippedBlock);
+
+            if (newState != null) {
+                cir.setReturnValue(Optional.of(newState));
+            }
+        }
+    }
+
+    @Unique
+    @Nullable
+    private static BlockState plantopia$getSpecialStrippedState(@NotNull BlockState unstrippedState, Block strippedBlock) {
+        if (unstrippedState.hasProperty(BlockStateProperties.FACING)) {
+            var axis = unstrippedState.getValue(BlockStateProperties.FACING).getAxis();
+            return strippedBlock.defaultBlockState().setValue(RotatedPillarBlock.AXIS, axis);
+        }
+
+        return null;
+    }
+}
