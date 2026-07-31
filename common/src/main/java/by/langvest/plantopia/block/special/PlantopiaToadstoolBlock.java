7f61f1840d257606f5dd6f5b3c81f7de12c0a236
LanGvest <mister.langvest@gmail.com>
2026-07-31 13:16:34 +0300

    🩹 Fix toadstool block spreading mechanics

--- common/src/main/java/by/langvest/plantopia/block/special/PlantopiaToadstoolBlock.java
+++ common/src/main/java/by/langvest/plantopia/block/special/PlantopiaToadstoolBlock.java
@@ -3,7 +3,9 @@ package by.langvest.plantopia.block.special;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
@@ -26,6 +28,41 @@ public class PlantopiaToadstoolBlock extends MushroomBlock {
 
     public @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (random.nextInt(25) != 0) {
+            return;
+        }
+
+        if (!isValidBrightnessToSpread(level.getRawBrightness(pos, 0))) {
+            return;
+        }
+
+        var mushroomLimit = 5;
+        for (var nearbyPos : BlockPos.betweenClosed(pos.offset(-4, -1, -4), pos.offset(4, 1, 4))) {
+            if (level.getBlockState(nearbyPos).is(this)) {
+                mushroomLimit--;
+                if (mushroomLimit <= 0) {
+                    return;
+                }
+            }
+        }
+
+        var candidatePos = new BlockPos.MutableBlockPos();
+        for (int i = 0; i < 4; i++) {
+            candidatePos.setWithOffset(pos, random.nextInt(3) - 1, random.nextInt(2) - random.nextInt(2), random.nextInt(3) - 1);
+
+            if (level.isEmptyBlock(candidatePos) && state.canSurvive(level, candidatePos) && isValidBrightnessToSpread(level.getRawBrightness(candidatePos, 0))) {
+                level.setBlock(candidatePos, state, Block.UPDATE_CLIENTS);
+                return;
+            }
+        }
+    }
+
+    protected boolean isValidBrightnessToSpread(int brightness) {
+        return brightness < 13;
     }
 
     @Override
