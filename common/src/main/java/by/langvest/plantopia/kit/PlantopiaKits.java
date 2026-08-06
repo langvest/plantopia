8cbff6e83752246927d1c583ab84c64e187d5d20
LanGvest <mister.langvest@gmail.com>
2026-08-06 13:04:59 +0300

    ✨ Add mangrove balk blocks

--- common/src/main/java/by/langvest/plantopia/kit/PlantopiaKits.java
+++ common/src/main/java/by/langvest/plantopia/kit/PlantopiaKits.java
@@ -37,6 +37,7 @@ public class PlantopiaKits {
     public static final PlantopiaExtraVanillaTreeKit SPRUCE = createExtraVanillaTreeKit("spruce");
     public static final PlantopiaExtraVanillaTreeKit ACACIA = createExtraVanillaTreeKit("acacia");
     public static final PlantopiaExtraVanillaTreeKit JUNGLE = createExtraVanillaTreeKit("jungle");
+    public static final PlantopiaExtraVanillaTreeKit MANGROVE = createExtraVanillaTreeKit("mangrove");
 
     public static final PlantopiaExtraVanillaBirchKit BIRCH = new PlantopiaExtraVanillaBirchKit(
         "birch",
