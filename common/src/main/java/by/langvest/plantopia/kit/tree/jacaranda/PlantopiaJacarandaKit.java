558bc39b6a1e27b93e24b31fc5b2cadda4cba0e6
LanGvest <mister.langvest@gmail.com>
2026-08-18 13:26:09 +0300

    ✨ Add jacaranda balk blocks

--- common/src/main/java/by/langvest/plantopia/kit/tree/jacaranda/PlantopiaJacarandaKit.java
+++ common/src/main/java/by/langvest/plantopia/kit/tree/jacaranda/PlantopiaJacarandaKit.java
@@ -5,6 +5,7 @@ import by.langvest.plantopia.block.special.PlantopiaJacarandaLeavesBlock;
 import by.langvest.plantopia.event.PlantopiaDatagenBridgeEvent;
 import by.langvest.plantopia.kit.config.PlantopiaTreeKitConfiguration;
 import by.langvest.plantopia.kit.special.PlantopiaAbstractTreeKit;
+import by.langvest.plantopia.kit.special.PlantopiaTreePlantKit;
 import by.langvest.plantopia.kit.special.PlantopiaTreeTrunkKit;
 import by.langvest.plantopia.kit.special.PlantopiaTreeStuffKit;
 import by.langvest.plantopia.meta.object.PlantopiaBlockMeta.MetaType;
@@ -20,6 +21,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class PlantopiaJacarandaKit extends PlantopiaAbstractTreeKit {
     public final PlantopiaTreeStuffKit stuff;
     public final PlantopiaTreeTrunkKit trunk;
+    public final PlantopiaTreePlantKit plant;
 
     public final PlantopiaJacarandaWorldgenKit worldgen;
 
@@ -39,6 +41,7 @@ public class PlantopiaJacarandaKit extends PlantopiaAbstractTreeKit {
         this.leaves = PlantopiaBlocks.registerBlock(baseName + "_leaves", PlantopiaJacarandaLeavesBlock::new, config.applyMeta(MetaType.LEAVES).mapColor(foliageColor));
 
         this.trunk = new PlantopiaTreeTrunkKit(baseName, config);
+        this.plant = new PlantopiaTreePlantKit(baseName, trunk.wood, trunk.strippedWood, config);
         this.stuff = new PlantopiaTreeStuffKit(baseName, woodType, config);
     }
 
@@ -60,6 +63,9 @@ public class PlantopiaJacarandaKit extends PlantopiaAbstractTreeKit {
         super.addRecipes(bridge);
 
         bridge.planksFromLogs(stuff.planks.get(), trunk.logsItemTag);
+        bridge.planksFromBalks(stuff.planks.get(), plant.balksItemTag);
         bridge.hangingSign(stuff.hangingSign.get(), trunk.strippedLog.get());
+        bridge.balksFromLogs(plant.balk.get(), trunk.log.get());
+        bridge.balksFromLogs(plant.strippedBalk.get(), trunk.strippedLog.get());
     }
 }
